f4860f249759a889af9b824c269148467af1b003
reatter <michael.lannert@gmail.com>
2018-03-20 22:29:22 +0100

    Diagramm 1.0 funzt

--- js/verbrauchsdiagramm.js
+++ js/verbrauchsdiagramm.js
@@ -1,48 +1,103 @@
 var Fahrer = {
-	id: undefined,
-	email: undefined
+	id : undefined,
+	email : undefined
 };
 
 var fahrtCounter = 0;
 
 function VerbrauchProFahrt(fahrt) {
 	this.id = ++fahrtCounter;
-	this.fahrzeugName
-	this.strecke = 
-	this.liter = 0;
+	this.fahrzeugName = fahrt.fahrzeugName;
+	this.strecke = fahrt.endKm - fahrt.startKm;
+	this.liter = fahrt.liter;
 }
 
-// single JSON for reference 
-// {"email":"test@test.de","startKm":"800.00","endKm":"1000.00","liter":"42.00","fahrzeugName":"Bentley"}
+// wie vom local Scope zum global scope?
+var FinalGraphData = [];
+
+function passGraphDataToHtml() {
+
+	var realPlot = document.getElementById('realPlot');
+
+	// Übergebe die Graph-Daten an das HTML
+
+	realPlot.FinalGraphData = FinalGraphData;
+}
 
 $(document).ready(function() {
 	Fahrer = JSON.parse(localStorage.getItem("spritrechner_fahrer"));
 	$("#btnGetStatistik").click(function(e) {
 		e.preventDefault(); // keine neue Seite
-		
+
 		$.ajax({
-	        type: "POST",
-	        url: "php/appinterface/request_handler.php",
-	        data: "&action=get_statistik_verbrauch&fahrer_id=" + Fahrer.id //fahrer_id aus dem localStorage holen
-	    }).done(function(response) {
-	        response = JSON.parse(response);
-	        if (response.istFehler) {
-	            $("#loginFehler").text("FEHLER!");
-	        } else {
-	        	// JSON-Object abspeichern 
-	        	for(var i = 0; i <= response.nachricht.length; i++) {
-	        		// in Object übergeben
-//	        		var fahrt = response.nachricht[i];
-//	        		
-//	        		var idString = JSON.stringify(fahrt.id);
-//	        		var nameString = JSON.stringify(fahrt.fahrzeugName);
-	        		
-	        		//console.log(idString+" "+ nameString);
-	        		console.log(JSON.stringify(response.nachricht[i]))
-	        	}
-	      
-	        }
-	   });
+			type : "POST",
+			url : "php/appinterface/request_handler.php",
+			data : "&action=get_statistik_verbrauch&fahrer_id=" + Fahrer.id // fahrer_id
+		// aus
+		// dem
+		// localStorage
+		// holen
+		}).done(function(response) {
+			response = JSON.parse(response);
+			if (response.istFehler) {
+				$("#loginFehler").text("FEHLER!");
+			} else {
+				// JSON-Object abspeichern
+
+				// Graph-Object erzeugen
+				var Graph = new Object;
+				var x = [];
+				var y = [];
+				Graph.x = x;
+				Graph.y = y;
+				Graph.type = 'bar';
+				
+				var fuckit = Array [ { } ]
+
+				// layout
+				var GraphLayout = {
+					title : 'Verbrauch pro Fahrt ',
+					xaxis : {
+						title : 'Fahrt',
+						showgrid : false,
+						zeroline : false
+					},
+					yaxis : {
+						title : 'Verbrauch in l/100km',
+						showline : false
+					}
+				};
+
+				for (var i = 0; i < response.nachricht.length; i++) {
+					// in Object übergeben
+					var fahrt = response.nachricht[i]; // kein JS-Object? Nur
+					// final variable!
+
+					var currentFahrt = JSON.parse(JSON.stringify(fahrt)); // "cast"
+					// to
+					// object
+
+					// Graph fuellen mit Daten
+					x.push(i+1);
+					y.push(100* currentFahrt.liter/(currentFahrt.endKm-currentFahrt.startKm));
+
+				}
+
+				// in Array packen für plot.ly
+				FinalGraphData.push(Graph);
+
+				console.log("FinalGraphData in JS-AJAX");
+				console.log(FinalGraphData);
+				console.log(" ");
+
+				console.log("Graph in JS-AJAX");
+				console.log(Graph);
+				console.log(" ");
+
+				Plotly.newPlot('realPlot', FinalGraphData, GraphLayout);
+
+			}
+		});
 	});
-	
+
 });
